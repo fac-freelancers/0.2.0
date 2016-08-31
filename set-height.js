708ce2f4f7e22c3c91ffe3df71e8621ce6ec9f3b
tu6619 <tu6619@my.bristol.ac.uk>
2016-08-31 17:25:27 +0100

    height of each section set by javascript function abstracted out to its own file

---
+++ set-height.js
@@ -0,0 +1,18 @@
+(function () {
+  function size () {
+    var section = document.getElementsByClassName('content-section');
+    var content = document.getElementsByClassName('content');
+
+    [].forEach.call(section, function (el) {
+      el.style.height = window.innerHeight + 'px';
+    });
+
+    [].forEach.call(content, function (el) {
+      el.style.marginTop = Math.floor(window.innerHeight / 3) + 'px';
+    });
+  }
+
+  size();
+
+  window.onresize = size;
+})();
